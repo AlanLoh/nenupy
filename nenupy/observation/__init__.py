b46128b32222a3837f350bcd6ecb0ca050df4575
Alan Loh <alan.loh.up7@gmail.com>
2020-11-12 16:45:29 +0100

    remove automated init of pyvo

--- nenupy/observation/__init__.py
+++ nenupy/observation/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-from .tapdatabase import ObsDatabase
+# from .tapdatabase import ObsDatabase
 from .sqldatabase import ParsetDataBase
 from .parset import Parset
 from .pointing import *
